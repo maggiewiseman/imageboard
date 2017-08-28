4fc2adec8e52e122a60e63cc22c416a36d0b7527
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-08-28 17:12:37 +0200

    (mew) json data renders to screen

--- server.js
+++ server.js
@@ -10,12 +10,17 @@ app.use(require('body-parser').urlencoded({
     extended: false
 }));
 
-app.get('/home', (request, response, next)=> {
+app.get('/home', (req, res, next)=> {
     return dbQ('getAllImages').then((results)=> {
         console.log('SERVER /home:', results);
         return formatHomeJSON(results);
     }).then((jsonResult)=>{
-        res.json(jsonResult);
+        res.json({
+            images: [
+                { title: 'title 1', userName: 'Disco Duck', desc: 'description of image 1' },
+                { title: 'title 2', userName: 'Funky Chicken', desc: 'description of image 2' }
+            ]
+        });
     }).catch(e => console.log(e.stack));
 });
 
@@ -27,7 +32,7 @@ app.listen(process.env.PORT || 8080, () => {
 
 function formatHomeJSON(rows) {
     //this function is going to return a json object to send in the response
-    // 
+    //
     // var formattedJsonArr;
     // rows.forEach(function(image) {
     //
