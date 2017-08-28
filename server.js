b010fbfa6f39ab994d274d2e6e16f242a4d8f725
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-08-28 16:22:21 +0200

    (mew) initial set up of home template

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express');
-var bodyParser = require('bodyParser');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser);
+app.use(require('body-parser').urlencoded({
+    extended: false
+}));
+
+app.get('/home', (request, response, next)=> {
+
+});
+
 
 app.listen(process.env.PORT || 8080, () => {
     console.log('listening on port 8080');
