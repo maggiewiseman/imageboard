1ae6783fe5b85bd58d1851f7397b96df67398a3f
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-08-28 17:44:48 +0200

    (mew) getting json back from db and putting it in the model

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,55 @@
 (function() {
+    Handlebars.templates = Handlebars.templates || {};
+
+    var templates = document.querySelectorAll('template');
+
+    Array.prototype.slice.call(templates).forEach(function(tmpl) {
+        Handlebars.templates[tmpl.id] = Handlebars.compile(tmpl.innerHTML.replace(/{{&gt;/g, '{{>'));
+    });
+
+    Handlebars.partials = Handlebars.templates;
 
     //make model
-    var Board = Backbone.Model.extend({
+    var BoardModel = Backbone.Model.extend({
         initialize: function() {
             this.fetch();
+            console.log('initializing after fetch');
+            console.log(this);
+
         },
         url: '/home'
 
     });
 
-    //instantiate model
-    var myBoard = new Board();
+    var myBoard = new BoardModel();
+myBoard.on('change', function() {
+        console.log('change event happened');
+        //view.render();
+        console.log(myBoard.get('images'));
+    });
+    // var BoardView = Backbone.View.extend({
+    //     initialize: function() {
+    //         console.log('initializing view');
+    //         var view = this;
+    //         this.model.on('change', function() {
+    //             console.log('change event happened');
+    //             view.render();
+    //             //console.log(this.model.get('images'));
+    //         });
+    //
+    //     },
+    //     render: function() {
+    //         console.log('views render function called');
+    //         var data = this.model.toJSON();
+    //         var html = Handlebars.templates.imageBoard(data);
+    //         this.$el.html(html);
+    //     }
+    // });
+    //
+    // var myBoardView = new BoardView({
+    //     el: '#main',
+    //     model: new BoardModel
+    // });
 
-       
+
 }());
