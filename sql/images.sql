acbfc2540e20cf48a49453828c902f3974773a92
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-08-29 15:42:01 +0200

    (mew)getting my uploaded image

--- sql/images.sql
+++ sql/images.sql
@@ -6,10 +6,10 @@ CREATE TABLE images(
     username VARCHAR(255) NOT NULL,
     title VARCHAR(255) NOT NULL,
     description TEXT,
-    likes INTEGER,  
+    likes INTEGER,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO images (image, username, title, description) VALUES ('MQwozP4QM5uK84XgPs4Q0oUIVWiwzN-w.jpg', 'funkychicken', 'Welcome to Berlin and the future!', 'This photo brings back so many great memories.');
-INSERT INTO images (image, username, title, description) VALUES ('wg8d94G_HrWdq7bU_2wT6Y6F3zrX-kej.jpg', 'discoduck', 'Elvis', 'We can''t go on together with suspicious minds.');
-INSERT INTO images (image, username, title, description) VALUES ('XCv4AwJdm6QuzjenFPKJocpipRNNMwze.jpg', 'discoduck', 'Hello Berlin', 'This is going to be worth a lot of money one day.');
+INSERT INTO images (image, username, title, description) VALUES ('x3hQUsyUdrUfU_g_SvUKw_jt93j6LWMf.png', 'funkychicken', 'Backbone Tutorial', 'Screenshot of backbone tutorial');
+-- INSERT INTO images (image, username, title, description) VALUES ('wg8d94G_HrWdq7bU_2wT6Y6F3zrX-kej.jpg', 'discoduck', 'Elvis', 'We can''t go on together with suspicious minds.');
+-- INSERT INTO images (image, username, title, description) VALUES ('XCv4AwJdm6QuzjenFPKJocpipRNNMwze.jpg', 'discoduck', 'Hello Berlin', 'This is going to be worth a lot of money one day.');
