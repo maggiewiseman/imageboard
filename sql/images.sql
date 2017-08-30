e9aac1f4c37bf4ba5997763d99e0cef15d89b861
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-08-30 15:20:39 +0200

    (mew) added comments table

--- sql/images.sql
+++ sql/images.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS images;
+DROP TABLE IF EXISTS comments;
 
 CREATE TABLE images(
     id SERIAL PRIMARY KEY,
@@ -10,6 +11,18 @@ CREATE TABLE images(
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    image_id INT REFERENCES images(id),
+    posted_by VARCHAR(255) NOT NULL,
+    comment TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 INSERT INTO images (image, username, title, description) VALUES ('x3hQUsyUdrUfU_g_SvUKw_jt93j6LWMf.png', 'funkychicken', 'Backbone Tutorial', 'Screenshot of backbone tutorial');
+
+INSERT INTO comments (image_id, posted_by, comment) VALUES ('1', 'Rocky Rhino', 'This image is amaze balls.');
+
+INSERT INTO comments (image_id, posted_by, comment) VALUES ('1', 'Gina Giraffe', 'I agree, this is totally rad.');
 -- INSERT INTO images (image, username, title, description) VALUES ('wg8d94G_HrWdq7bU_2wT6Y6F3zrX-kej.jpg', 'discoduck', 'Elvis', 'We can''t go on together with suspicious minds.');
 -- INSERT INTO images (image, username, title, description) VALUES ('XCv4AwJdm6QuzjenFPKJocpipRNNMwze.jpg', 'discoduck', 'Hello Berlin', 'This is going to be worth a lot of money one day.');
